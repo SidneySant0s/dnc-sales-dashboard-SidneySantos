029895266295c462fe74b516631a332eb801e953
Sidney Santos <sidney.santos.zx@gmail.com>
2025-10-19 03:20:27 -0300

    Feat: Create Themes/correcao do erro darktheme

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { ThemeProvider } from 'styled-components'
 import  ReactDOM  from 'react-dom/client'
 import App from './App.tsx'
-import { darkTheme, lighTheme, GlobalStyle } from './styles';
+import { lighTheme, GlobalStyle } from './styles';
 
  ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
