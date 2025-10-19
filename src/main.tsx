965a7f75b36c73a0ba256b96879daf76ce315616
Sidney Santos <sidney.santos.zx@gmail.com>
2025-10-19 03:17:04 -0300

    Feat: Create Themes

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { ThemeProvider } from 'styled-components'
 import  ReactDOM  from 'react-dom/client'
 import App from './App.tsx'
+import { darkTheme, lighTheme, GlobalStyle } from './styles';
 
  ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme = {lighTheme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
