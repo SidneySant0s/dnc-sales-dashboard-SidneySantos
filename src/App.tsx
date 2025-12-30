4dcb1dd1ad58a3a0ec053b1b2fecbaf754a8e9c9
Sidney Santos <sidney.santos.zx@gmail.com>
2025-12-30 17:25:53 -0300

    Resolve conflito em App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,8 @@ import Cookies from "js-cookie"
 import { Home, Leads, Login, Profile, Registration} from "./pages"
 
 function App() {
-<<<<<<< HEAD
+  
   const ProtectedRoute = () => {
-=======
-  const ProtectedRouted = () => {
->>>>>>> 0c988019228c04489cffe701b8218bf55dc64b61
     const checkAuthCookie = Cookies.get('Authorization')
     if (!checkAuthCookie){
       alert('Autenticação necessária')
@@ -22,11 +19,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Login/>}/>
         <Route path="/cadastro" element={<Registration/>}/>
-<<<<<<< HEAD
+
         <Route element={<ProtectedRoute />}>
-=======
-        <Route element={<ProtectedRouted />}>
->>>>>>> 0c988019228c04489cffe701b8218bf55dc64b61
+
           <Route path="/home" element={<Home/>}/>
           <Route path="/leads" element={<Leads/>}/>
           <Route path="/Perfil" element={<Profile/>}/>
