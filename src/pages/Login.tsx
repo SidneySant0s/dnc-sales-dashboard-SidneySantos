45cf5ec9ffe0f3ab683764513641f970c3be51a6
Sidney <sidney.santos.zx@gmail.com>
2026-01-20 13:48:13 -0300

    Fix: corrigindo erro no fluxo de perfil

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import {  use, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { jwtDecode } from 'jwt-decode';
 import Cookies from 'js-cookie';
