ad5096ac0a74eec789108c0ad29c4adddd27beec
Sidney <sidney.santos.zx@gmail.com>
2026-01-20 13:37:40 -0300

    Feat: Create profile flow

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,16 @@ import  ReactDOM  from 'react-dom/client'
 import App from './App.tsx'
 import { GlobalStyle } from './styles/globalStyle.ts'
 import { AppthemeProvider } from './contexts/AppThemeContext.tsx'
+import { Provider } from 'react-redux'
+import  store  from './redux/index.ts'
 
  ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AppthemeProvider >
+    <Provider store={store}>
+      <AppthemeProvider >
       <GlobalStyle />
       <App />
     </AppthemeProvider>
+    </Provider>
   </React.StrictMode>
 )
